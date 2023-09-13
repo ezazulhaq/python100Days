75f2aa499a580c1e975cd262f8edbb94fd184a94
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2023-09-13 17:43:41 +0530

    day 17 quiz project completed with updated quiz data using TRIVIA DB url - https://opentdb.com/api_config.php
    Code refractored

--- day017/quiz-game-start/question_model.py
+++ day017/quiz-game-start/question_model.py
@@ -1,4 +1,4 @@
 class Question:
-    def __init__(self, question, correct_answer):
-        self.question = question
+    def __init__(self, question_text, correct_answer):
+        self.question_text = question_text
         self.correct_answer = correct_answer
--- day017/quiz-game-start/quiz_brain.py
+++ day017/quiz-game-start/quiz_brain.py
@@ -8,7 +8,7 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.question} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.question_text} (True/False): ")
         self.check_answer(user_answer, current_question.correct_answer)
 
     def still_has_questions(self):
